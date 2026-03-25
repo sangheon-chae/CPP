9f62313bdef7b333d514128f3ad06fa0d4f66422
chaesangheon <ctgccwcwdddt2@gmail.com>
2026-03-25 14:40:00 +0900

    Week

---
+++ Week4/p197_2/2.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+int reverse(int n);
+int main()
+{
+    int n;
+    int input;
+    int output;
+    
+    printf("숫자를 입력하세요: ");
+    scanf("%d", &input);
+    
+    output = reverse(input);
+
+    printf("처음 숫자: %d\n", input);
+    printf("뒤집힌 숫자: %d\n", output);
+    return 0;
+}
+
+
+
+
+int reverse(int n)
+{
+    
+    int result = 0;
+    while (n > 0)
+    
+    {
+        int lastnum = n % 10;
+        result = result * 10 + lastnum;
+        n = n / 10;
+    }
+    
+    return result;
+}
