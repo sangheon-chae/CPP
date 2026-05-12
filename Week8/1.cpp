38efcc4e8ce37660577d1bc37e13f97423fa205f
chaesangheon <ctgccwcwdddt2@gmail.com>
2026-05-12 11:50:13 +0900

    Week

---
+++ Week8/1.cpp
@@ -0,0 +1,13 @@
+#include <time.h>
+#include <iostream>
+#include <string>
+
+
+class MyTime {
+    int hour, minute, second, msec;
+public:
+    MyTime(int h = 0, int m = 0, int s = 0, int ms = 0) : hour(h), minute(m), second(s), msec(ms) { }
+
+};
+
+
